433c15098fd5205fb0a8d598c646085c76c975ca
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-06-19 03:31:55 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Вывести произведение чисел от 1 до N.
+
+int Func(int N)
+{
+    int multiplication = 1;
+    for (int i = 1; i <= N ; i++)
+    {
+        multiplication *= i;
+    }
+    return multiplication;
+}
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+
+if (N <= 0)
+{
+    Console.WriteLine("Введите положительное число");
+}
+else
+{
+    Console.WriteLine(Func(N));
+}
